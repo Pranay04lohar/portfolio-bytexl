a76b86d499f128e9d50e1adf40dbc99fb03d0267
Pranay04lohar <codepranay10@gmail.com>
2025-06-12 13:51:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("magicButton").addEventListener("click", function () {
+  const message = document.getElementById("message");
+  message.textContent = "✨ Magic happened! ✨";
+});
